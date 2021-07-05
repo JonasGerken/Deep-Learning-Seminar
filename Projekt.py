89226f54ac1106a96a5e19737244bbc595da8ad9
JonasGerken <43642021+JonasGerken@users.noreply.github.com>
2021-07-05 17:18:28 +0200

    Update Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -11,8 +11,7 @@ from sklearn.metrics import accuracy_score,
 confusion_matrix
 
 #Read the data
-df=pd.read_csv('C:\\Users\\Jonas Gerken
-\\Desktop\\news.csv')
+df=pd.read_csv('C:\\Users\\Jonas Gerken\\Desktop\\news.csv')
 
 #Get shape and head
 df.shape
@@ -23,26 +22,21 @@ labels=df.label
 labels.head()
 
 #DataFlair - Split the dataset
-x_train,x_test,y_train,y_test=train_test_split
-(df['text'], 
+x_train,x_test,y_train,y_test=train_test_split(df['text'], 
 labels, test_size=0.2, random_state=7)
 
 #DataFlair - Initialize a TfidfVectorizer
-tfidf_vectorizer=TfidfVectorizer(stop_words=
-'english', max_df=0.7)
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
 
-#DataFlair - Fit and transform train set, 
-#transform test set
+#DataFlair - Fit and transform train set, transform test set
 tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
 tfidf_test=tfidf_vectorizer.transform(x_test)
 
-#DataFlair - Initialize a PassiveAggressive
-#Classifier
+#DataFlair - Initialize a PassiveAggressiveClassifier
 pac=PassiveAggressiveClassifier(max_iter=50)
 pac.fit(tfidf_train,y_train)
 
-#DataFlair - Predict on the test set and calculate 
-#accuracy
+#DataFlair - Predict on the test set and calculate accuracy
 y_pred=pac.predict(tfidf_test)
 score=accuracy_score(y_test,y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
